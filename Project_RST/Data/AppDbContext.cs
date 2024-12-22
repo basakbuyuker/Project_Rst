9fb68f0926635789cc9a69f0a1644c7dc35d1093
monster <monster@BBAUSY>
2024-12-23 00:48:37 +0300

    Proje dosyası ekle.

---
+++ Project_RST/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_RST.Data
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ Project_RST/Data/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace Project_RST.Data.Models
+{
+    public class Task
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150), Column(TypeName = "nvarchar(150)")]
+        public string Title { get; set; } = "Title";
+
+        [MaxLength(250), Column(TypeName = "nvarchar(250)")]
+        public string Description { get; set; } = "Description";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public bool Done { get; set; } = false;
+        public bool Deleted { get; set; } = false;
+
+
+    }
+}
